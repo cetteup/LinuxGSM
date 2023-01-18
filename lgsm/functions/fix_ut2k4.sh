3eb9d7f3c93fa292fa27787e17635f4fbc48f51d
cetteup <me@cetteup.com>
2023-01-18 16:32:59 +0100

    feat(ut2k4): Use OpenSpy master instead of soon defunct Epic master closes #4086

--- lgsm/functions/fix_ut2k4.sh
+++ lgsm/functions/fix_ut2k4.sh
@@ -16,6 +16,11 @@ echo -e "applying WebAdmin CharSet fix."
 echo -e "http://forums.tripwireinteractive.com/showpost.php?p=442340&postcount=1"
 sed -i 's/CharSet="iso-8859-1"/CharSet="utf-8"/g' "${systemdir}/UWeb.int"
 fn_sleep_time
+echo -e "applying OpenSpy master server fix."
+echo -e "http://beta.openspy.net/en/howto/ut2k-engine/ut2004"
+sed -i 's/MasterServerList=(Address="ut2004master1.epicgames.com",Port=28902)/MasterServerList=(Address="utmaster.openspy.net",Port=28902)/' "${systemdir}/UT2004.ini"
+sed -i '/MasterServerList=(Address="ut2004master2.epicgames.com",Port=28902)/d' "${systemdir}/UT2004.ini"
+fn_sleep_time
 echo -e "applying server name fix."
 fn_sleep_time
 echo -e "forcing server restart."
